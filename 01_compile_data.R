13d39db6eec82f4a7aa08bdc97afd940e57c78ae
KateMMiller <kathryn_miller@nps.gov>
2025-11-14 16:01:47 -0500

    coef of wet

--- 01_compile_data.R
+++ 01_compile_data.R
@@ -29,3 +29,40 @@ ggplot(vmmi, aes(x = Year, y = vmmi, group = Code)) +
 # good > 65.22746
 # fair 52.785 - 65.22746
 # poor < 52.785
+
+head(VIEWS_RAM$species_list)
+head(VIEWS_RAM$tlu_Plant)
+View(VIEWS_RAM$tlu_Plant)
+
+
+spp <- left_join(VIEWS_RAM$species_list,
+                 VIEWS_RAM$tlu_Plant[,c("TSN", "Coef_wetness")],
+                 by = "TSN")
+
+sum_spp <- spp |> group_by(Code, Year) |>
+  summarize(mean_wet = mean(Coef_wetness, na.rm = T),
+            num_inv = sum(Invasive, na.rm = T),
+            num_exo = sum(Exotic, na.rm = T),
+            .groups = "drop")
+range(sum_spp$mean_wet)
+
+ggplot(sum_spp, aes(x = Year, y = mean_wet, group = Code)) +
+  geom_point() + geom_line() + facet_wrap(~Code) +
+  theme_bw() + labs(y = "Mean Wetness") +
+  ylim(-5, 1) +
+  geom_rect(aes(xmin = 2012, xmax = 2025, ymin = -5, ymax = -3),
+            fill = "dodgerblue4", alpha = 0.2) +
+  geom_rect(aes(xmin = 2012, xmax = 2025, ymin = -3, ymax = 0),
+            fill = "dodgerblue2", alpha = 0.2) +
+  geom_rect(aes(xmin = 2012, xmax = 2025, ymin = 0, ymax = 1),
+            fill = "forestgreen", alpha = 0.2) +
+  geom_point() + geom_line() + facet_wrap(~Code)
+
+table(spp$Coef_wetness)
+
+# Coef of Wetness
+# -5 Obligate wetland
+# -3 Facultative wetland
+# 0 Facultative
+# 3 Facultative upland
+# 5 Obligate upland
