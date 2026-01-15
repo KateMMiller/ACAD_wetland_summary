0f60574f9a8fa746f5ff39643047a9399b3bb8c5
KateMMiller <kathryn_miller@nps.gov>
2026-01-15 14:00:23 -0500

    updated figs

--- 03_ACAD_vs_NWCA.R
+++ 03_ACAD_vs_NWCA.R
@@ -1,12 +1,18 @@
 library(tidyverse)
 
+thresh <- c(41.48136, 60.94853)
+
 acad_ref <- read.csv("./results/Vegetation_MMI_2011-2021_ACAD_REF.csv") |>
   select(SITE_ID, YEAR, vmmi, vmmi_rating) |>
   mutate(site_type = "ACAD Sent.")
 
 head(acad_ref)
 
-nwca_prob <- read.csv("./results/Vegetation_MMI_2011-2021_EPA_PROB.csv") |>
+nwca_prob1 <- read.csv("./results/Vegetation_MMI_2011-2021_EPA_PROB.csv")
+
+table(nwca_prob1$US_L3CODE)
+
+nwca_prob <- nwca_prob1 |>
   select(SITE_ID, YEAR, vmmi, vmmi_rating) |>
   mutate(site_type = "EPA Prob.")
 
@@ -28,6 +34,11 @@ vmmi_comb$site_type_fac <- factor(vmmi_comb$site_type,
 ggplot(vmmi_comb, aes(x = site_type_fac, y = vmmi)) +
   geom_boxplot() + theme_classic() +
   geom_jitter(alpha = 0.2) +
-  labs(x = "Site Disturbance Type", y = "Veg. MMI")
+  labs(x = "Site Disturbance Type", y = "Veg. MMI") +
+  geom_hline(yintercept = thresh[1], linewidth = 0.1, color = "#696969",
+             linetype = 'dashed') +
+  geom_hline(yintercept = thresh[2], linewidth = 0.05, color = "#696969") +
+  labs(y = "Vegetation MMI", x = NULL)
 
-ggsave("./results/VMMI_distribution_site_type.png", height = 8, width = 10)
+
+ggsave("./results/VMMI_distribution_site_type_ACAD_EPA.png", height = 8, width = 10)
